606c2b6d8dea1233d838805c5e50067975d67b6d
benjamin.palfi <p4lf1ben1@gmail.com>
2020-07-23 22:37:05 +0200

    priest fear ward

--- priest/priest_fear_ward.lua
+++ priest/priest_fear_ward.lua
@@ -7,13 +7,3 @@ function fear_ward_tanks()
 	end
 	return nil
 end
-
-
-function fear_ward_tanks_reverse()
-    for i=1, #tank_list do
-        exact_target_by_name(mytable[#mytable + 1 - i])
-        if not UnitName("targettarget") == nil then
-            cast_buff("Spell_Holy_Excorcism", "Fear Ward")
-        end
-    end
-end
